9e01d0de903e70a27a4ff4af86b7eb432cae646d
Caleb Champlin <caleb.champlin@gmail.com>
2014-07-31 15:43:21 -0600

    Start adding requirements

---
+++ CustomActions/ClosePromptForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomActions
+{
+    public partial class ClosePromptForm : Form
+    {
+        public ClosePromptForm(string text)
+        {
+            InitializeComponent();
+            messageText.Text = text;
+        }
+
+        private void OkButtonClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
